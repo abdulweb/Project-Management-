65fec8bb94804683bbbfe08aca13ab886983a5de
Olami001 <abdulrasheeda9@gmail.com>
2019-01-20 08:06:23 +0100

    comapny table adjust

--- database/migrations/2019_01_18_165752_create_companies_table.php
+++ database/migrations/2019_01_18_165752_create_companies_table.php
@@ -15,7 +15,7 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-            $tbale->string('name');
+            $table->string('name');
             $table->longText('description')->nullable();
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
