fcfecd3bbd71549619d4294ee61c31f39a2ee2e0
Olami001 <abdulrasheeda9@gmail.com>
2019-01-29 10:10:37 +0100

    Relationship add to Project

--- app/Project.php
+++ app/Project.php
@@ -14,4 +14,12 @@ class Project extends Model
         'user_id',
         'days',
     ];
+
+    public function users(){
+        return $this->belongTo('App\Models\User');
+    }
+
+    public function comapnies(){
+        return $this->belongTo('App\Models\Company');
+    }
 }
