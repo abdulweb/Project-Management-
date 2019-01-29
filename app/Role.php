a4003983bf1cd5504c3507efb5eeb5c490017db0
Olami001 <abdulrasheeda9@gmail.com>
2019-01-29 10:10:46 +0100

    Relationship add to Role

--- app/Role.php
+++ app/Role.php
@@ -10,4 +10,9 @@ class Role extends Model
      protected $fillable =[
         'name',
     ];
+
+    //Relationship
+    public function users(){
+        return $this->hasMany('App\Models\User');
+    }
 }
